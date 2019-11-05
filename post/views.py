19893d06a1f86e68087b57b1245b440efccaae4f
David Duefrene <fortismilite@gmail.com>
2019-11-04 23:49:10 -0700

    pagination working

--- post/views.py
+++ post/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from django.views.generic import ListView
+from .models import Post
 
-# Create your views here.
+def post_list(request):
+    object_list = Post.published.all()
+    paginator = Paginator(object_list, 3) # 3 posts in each page
+    page = request.GET.get('page')
+    try:
+        posts = paginator.page(page)
+    except PageNotAnInteger:
+        # If page is not an integer deliver the first page
+        posts = paginator.page(1)
+    except EmptyPage:
+        # If page is out of range deliver last page of results
+        posts = paginator.page(paginator.num_pages)
+    
+    return render(request,
+                  'post/content/list.html',
+                  {'page': page,
+                   'posts': posts})
+
+def post_detail(request, year, month, day, post):
+    post = get_object_or_404(Post, slug=post,
+                                   status='published',
+                                   publish__year=year,
+                                   publish__month=month,
+                                   publish__day=day)
+    return render(request,
+                  'post/content/detail.html',                               
+                  {'post': post,})
